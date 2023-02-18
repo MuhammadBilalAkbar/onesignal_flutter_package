9d765eb841d455efcde72ba2e5cd23c7689513ca
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-02-19 00:15:30 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,11 +12,14 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
           appBarTheme: const AppBarTheme(
-            titleTextStyle: TextStyle(fontSize: 24.0),
+            titleTextStyle: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
             centerTitle: true,
           ),
           textTheme: const TextTheme(
-            titleLarge: TextStyle(fontSize: 24.0),
+            bodyMedium: TextStyle(fontSize: 30.0),
           ),
         ),
       );
@@ -32,7 +33,7 @@ class OneSignalFlutter extends StatefulWidget {
 }
 
 class OneSignalFlutterState extends State<OneSignalFlutter> {
-  static const oneSignalAppId = 'd7509317-3652-4ff2-aea7-6db90d693a51';
+  final oneSignalAppId = 'd7509317-3652-4ff2-aea7-6db90d693a51';
 
   @override
   void initState() {
@@ -45,12 +46,8 @@ class OneSignalFlutterState extends State<OneSignalFlutter> {
         appBar: AppBar(
           title: const Text('OneSignal Flutter'),
         ),
-        body: Center(
-          child: Text(
-            'OneSignal PushNotification',
-            style: TextStyle(
-                fontSize: Theme.of(context).textTheme.titleLarge!.fontSize),
-          ),
+        body: const Center(
+          child: Text('OneSignal PushNotification'),
         ),
       );
 }
